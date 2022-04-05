5be4688cf673a11b6c3c5203759eda47b6175241
akash1331 <akashpk1331@gmail.com>
2022-04-05 22:09:57 +0530

    foreign key fixed

---
+++ PerApp/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from PerApp.models import permission
+
+class PermissionSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = permission
+        fields = '__all__'
+    
+
+
+class admingrantSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = permission
+        fields = ('id','student_roll','granted','reason',"from_time") #updated
